6f46a975941cc1a8502eb3dd5f25eb79670b92d4
mni-dev <nazrulislam01531@gmail.com>
2022-04-10 00:09:33 +0600

    protected page/component added

--- src/App.js
+++ src/App.js
@@ -12,17 +12,21 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-       <Routes>
-         <Route path='/' element={<Home></Home>}></Route>
-         <Route path='/login' element={<Login></Login>}></Route>
-         <Route path='/register' element={<Register></Register>}></Route>
-         <Route path='/products' element={<Products></Products>}></Route>
-         <Route path='/orders' element={
-           <RequireAuth>
-             <Orders></Orders>
-           </RequireAuth>
-         }></Route>
-       </Routes>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/products' element={
+          <RequireAuth>
+            <Products></Products>
+          </RequireAuth>
+        }></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>
+        }></Route>
+      </Routes>
     </div>
   );
 }
